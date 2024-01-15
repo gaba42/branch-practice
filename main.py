aa6a4c28a6803111af5cd44971256e8c4fb0f4bf
gaba42 <106816837+gaba42@users.noreply.github.com>
2024-01-15 12:05:33 +0900

    feat: Change in in range() 6 to 7

---
+++ main.py
@@ -0,0 +1,12 @@
+from random import randint
+from random import choice
+
+def get_randint() -> list:
+    return [randint(1,45) for _ in range(7)]
+
+def get_choice() -> list:
+    return [choice(range(1,45+1)) for _ in range(7)]
+
+if __name__ == '__main__':
+    print(get_randint())
+    print(get_choice())
